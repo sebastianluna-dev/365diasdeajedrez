a3c456a75b092a1a027a74baad3af2b595a886f5
Sebastian Luna <sebastian.luna.developer@gmail.com>
2026-08-04 17:17:20 -0600

    create program modules data and interface files

---
+++ interfaces/programModule.ts
@@ -0,0 +1,9 @@
+export interface ProgramModule {
+  durationLabel: string;
+  moduleNumber: number;
+  totalModules: number;
+  title: string;
+  subtitle: string;
+  description: string;
+  topics: string[];
+}
