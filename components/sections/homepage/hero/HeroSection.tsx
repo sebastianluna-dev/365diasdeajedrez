da46e438b99a30eb8b39d80cd955d1c20383fda4
Sebastian Luna <sebastian.luna.developer@gmail.com>
2026-08-04 15:55:00 -0600

    create orbit component for homepage hero section

--- components/sections/homepage/hero/HeroSection.tsx
+++ components/sections/homepage/hero/HeroSection.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { SiteHeader } from "../../../layout/SiteHeader";
 import { FloatBadge } from "./FloatBadge";
+import { Orbit } from "./Orbit";
 import { ChessPieceUnicode } from "@/enums/chessPieces";
 import "./hero.css";
 
@@ -50,19 +51,7 @@ export function HeroSection() {
                     />
                   </div>
                 </div>
-                <div
-                  className="orbit1 relative"
-                  style={{ gridArea: "1 / 1 / 2 / 2" }}
-                >
-                  <div className="w-2 h-2 bg-primary rotate-0 absolute rounded-full -top-[5px] right-1/2"></div>
-                  <div className="w-1.5 h-1.5 bg-secondary rotate-0 absolute rounded-full top-1/2 -right-[4px]"></div>
-                  <div className="w-1.5 h-1.5 bg-white rotate-0 absolute rounded-full right-1/2 -bottom-[5px]"></div>
-                </div>
-
-                <div className="orbit2" style={{ gridArea: "1 / 1 / 2 / 2" }}>
-                  <div className="w-2 h-2 bg-white rotate-0 absolute rounded-full -top-[5px] right-1/2"></div>
-                  <div className="w-1.5 h-1.5 bg-white rotate-0 absolute rounded-full -bottom-[5px] right-1/2"></div>
-                </div>
+                <Orbit />
               </div>
             </div>
 
---
+++ components/sections/homepage/hero/Orbit.tsx
@@ -0,0 +1,16 @@
+export function Orbit() {
+  return (
+    <>
+      <div className="orbit1 relative" style={{ gridArea: "1 / 1 / 2 / 2" }}>
+        <div className="w-2 h-2 bg-primary rotate-0 absolute rounded-full -top-[5px] right-1/2"></div>
+        <div className="w-1.5 h-1.5 bg-secondary rotate-0 absolute rounded-full top-1/2 -right-[4px]"></div>
+        <div className="w-1.5 h-1.5 bg-white rotate-0 absolute rounded-full right-1/2 -bottom-[5px]"></div>
+      </div>
+
+      <div className="orbit2" style={{ gridArea: "1 / 1 / 2 / 2" }}>
+        <div className="w-2 h-2 bg-white rotate-0 absolute rounded-full -top-[5px] right-1/2"></div>
+        <div className="w-1.5 h-1.5 bg-white rotate-0 absolute rounded-full -bottom-[5px] right-1/2"></div>
+      </div>
+    </>
+  );
+}
