49f449b1051c399856f5b984afafc92e78843e0a
Sebastian Luna <sebastian.luna.developer@gmail.com>
2026-08-27 17:14:40 -0600

    feat: update article content layout with improved structure and styling

--- components/sections/blog-article/article-content/article-content.section.tsx
+++ components/sections/blog-article/article-content/article-content.section.tsx
@@ -14,42 +14,44 @@ export function ArticleContent({ article }: ArticleContentProps) {
 
   return (
     <article className="article-content">
-      <nav className="article-content__breadcrumb">
-        <Link href="/" className="article-content__breadcrumb-link">
-          Inicio
-        </Link>
-        <span className="article-content__breadcrumb-sep">›</span>
-        <Link href="/blog" className="article-content__breadcrumb-link">
-          Blog
-        </Link>
-        <span className="article-content__breadcrumb-sep">›</span>
-        <span className="article-content__breadcrumb-current">{article.category}</span>
-      </nav>
+      <div className="article-content__inner">
+        <nav className="article-content__breadcrumb">
+          <Link href="/" className="article-content__breadcrumb-link">
+            Inicio
+          </Link>
+          <span className="article-content__breadcrumb-sep">›</span>
+          <Link href="/blog" className="article-content__breadcrumb-link">
+            Blog
+          </Link>
+          <span className="article-content__breadcrumb-sep">›</span>
+          <span className="article-content__breadcrumb-current">{article.category}</span>
+        </nav>
 
-      <h1 className="article-content__title">{article.title}</h1>
+        <h1 className="article-content__title">{article.title}</h1>
 
-      {author && (
-        <div className="article-content__byline">
-          <span className="article-content__avatar">{author.name.charAt(0)}</span>
-          <span>
-            <span className="article-content__author-name">{author.name}</span>
-            <span className="article-content__author-meta">{article.meta}</span>
-          </span>
+        {author && (
+          <div className="article-content__byline">
+            <span className="article-content__avatar">{author.name.charAt(0)}</span>
+            <span>
+              <span className="article-content__author-name">{author.name}</span>
+              <span className="article-content__author-meta">{article.meta}</span>
+            </span>
+          </div>
+        )}
+
+        <div className="article-content__divider" />
+
+        <div className="article-content__cover">
+          <Image
+            className="article-content__cover-image"
+            src={article.image.src}
+            alt={article.image.alt}
+            fill
+            sizes="(max-width: 820px) 100vw, 684px"
+          />
         </div>
-      )}
-
-      <div className="article-content__divider" />
-
-      <div className="article-content__cover">
-        <Image
-          className="article-content__cover-image"
-          src={article.image.src}
-          alt={article.image.alt}
-          fill
-          sizes="(max-width: 820px) 100vw, 684px"
-        />
+        <RichText className="article-body" data={article.content} converters={articleRichTextConverters} />
       </div>
-      <RichText className="article-body" data={article.content} converters={articleRichTextConverters} />
     </article>
   );
 }
