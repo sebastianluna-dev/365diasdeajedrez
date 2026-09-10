5e96e106b7021a5c95de48d065453b334d8ac5e0
Sebastian Luna <sebastian.luna.developer@gmail.com>
2026-09-10 01:45:24 -0600

    refactor: update site configuration types and improve documentation references

--- config/site.config.ts
+++ config/site.config.ts
@@ -1,4 +1,4 @@
-import type { SiteConfig } from "@/interfaces/site-config.interface";
+import type { SiteConfig } from "@/types/site-config.types";
 
 export const siteConfig: SiteConfig = {
   home: {
